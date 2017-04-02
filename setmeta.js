c54ff4a7c1ce3850f3ab49a6738fc3dff24d776e
Isidro Torregrosa Torralba <isistorre95@gmail.com>
2017-04-02 23:59:48 +0200

    Update setmeta.js

--- setmeta.js
+++ setmeta.js
@@ -1,9 +1,7 @@
 import { createMeta } from './createmeta';
 export const setMeta = function (target, keys, value) {
 
-    // if (!target) throw new Error('Target does not exist');
-    // if (!keys) throw new Error('attr does not exist');
-    // if (typeof value !== 'boolean' && !value) throw new Error('Invalid value');
+    //TODO: validate input parameters
 
     let meta = createMeta(target);
     keys = keys.split('.');
