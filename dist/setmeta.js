e017367a2244c3ebe1004783ecbb65ed03481651
Isidro Torregrosa Torralba <itorregrosa@alfatecsistemas.es>
2017-05-24 12:26:32 +0200

    update dist

--- dist/setmeta.js
+++ dist/setmeta.js
@@ -3,25 +3,15 @@
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
-exports.insertMeta = exports.setMeta = undefined;
+exports.setMeta = undefined;
 
 var _createmeta = require('./createmeta');
 
-var setMeta = exports.setMeta = function setMeta(target, keys, value, inArray) {
+var setMeta = exports.setMeta = function setMeta(target, keys, value) {
     var meta = (0, _createmeta.createMeta)(target);
-    insertMeta(meta, keys, value, inArray);
-};
-
-var insertMeta = exports.insertMeta = function insertMeta(meta, keys, value, inArray) {
-    meta = meta || {};
     keys = keys.split('.');
     keys.reduce(function (pre, cur, index) {
-        if (inArray) {
-            if (index !== keys.length - 1) pre[cur] = pre[cur] || {};else {
-                pre[cur] = pre[cur] || [];
-                pre[cur].push(value);
-            }
-        } else index !== keys.length - 1 ? pre[cur] = pre[cur] || {} : pre[cur] = value;
+        index !== keys.length - 1 ? pre[cur] = pre[cur] || {} : pre[cur] = value;
         return pre[cur];
     }, meta);
 };
