2a02db4239e4062d4fa09b4641307078b35441d0
Isidro Torregrosa Torralba <isistorre95@gmail.com>
2017-05-25 10:52:29 +0200

    Revert "remove inArray option from setMeta"
    This reverts commit 1d15f5fac29cff6d4b505e6159f80031d9b8fa37.

--- src/setmeta.js
+++ src/setmeta.js
@@ -1,10 +1,23 @@
 import { createMeta } from './createmeta';
 
-export const setMeta = function (target, keys, value) {
+export const setMeta = function (target, keys, value, inArray) {
     let meta = createMeta(target);
+    insertMeta(meta, keys, value, inArray);
+}
+
+export const insertMeta = function (meta, keys, value, inArray) {
+    meta = meta || {};
     keys = keys.split('.');
     keys.reduce((pre, cur, index) => {
-        (index !== keys.length - 1) ? pre[cur] = (pre[cur] || {}) : pre[cur] = value;
+        if (inArray) {
+            if (index !== keys.length - 1) pre[cur] = (pre[cur] || {});
+            else {
+                pre[cur] = (pre[cur] || []);
+                pre[cur].push(value);
+            }
+        }
+        else
+            (index !== keys.length - 1) ? pre[cur] = (pre[cur] || {}) : pre[cur] = value;
         return pre[cur];
     }, meta);
 }
